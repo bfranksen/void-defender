ae8a76c9c9b8ad900c644b52b9c5187a08806695
Braden Franksen <bfranksen93@gmail.com>
2021-04-26 02:16:22 -0700

    Updated player ship art and all projectile art.

--- Void Defender/Assets/Game/Scripts/Weapons/Spinner.cs
+++ Void Defender/Assets/Game/Scripts/Weapons/Spinner.cs
@@ -5,8 +5,25 @@ using UnityEngine;
 public class Spinner : MonoBehaviour {
 
     [SerializeField] float speedOfSpin = 360f;
+    private bool isBomb = false;
+
+    private void Start() {
+        if (gameObject.tag == "Bomb") {
+            isBomb = true;
+            Vector3 target = FindObjectOfType<Player>().transform.position;
+            target.z = 0f;
+            Vector3 objectPos = transform.position;
+            target.x = target.x - objectPos.x;
+            target.y = target.y - objectPos.y;
+            float angle = Mathf.Atan2(target.y, target.x) * Mathf.Rad2Deg;
+            angle += 90f;
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+        }
+    }
 
     private void Update() {
-        transform.Rotate(0, 0, speedOfSpin * Time.deltaTime);
+        if (!isBomb) {
+            transform.Rotate(0, 0, speedOfSpin * Time.deltaTime);
+        }
     }
 }
